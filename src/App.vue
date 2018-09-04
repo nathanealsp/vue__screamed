9303f2fca4fc87c824bdc85ea4cfa4e0021f61f3
Nytheneals <nythes@gmail.com>
2018-09-04 00:58:45 -0400

    Created First Component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
+    <Header greeting="Largest Movie Database"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  // data,
+  // methods,
+  // props
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+<header>Streamed</header>
+<p>{{greeting}}</p>
+<button type="submit" v-on:click="logger">Log Watever</button>
+ <ol>
+    <li v-for="todo in todos">
+      {{ todo.name }}
+    </li>
+  </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      todos: [
+        { name: "PWA" },
+        { name: "SSR" },
+        { name: "VUE" },
+        { name: "PYTHON" }
+      ]
+    };
+  },
+  props: {
+    greeting: String
+  },
+  methods: {
+    logger: function() {
+      console.log(this);
+      console.log(this.todos.map(item => item.name));
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
